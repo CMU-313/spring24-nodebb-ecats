d8ca1b65dc6d06104d96a2a3254999241501513e
SongEmily <emsong@andrew.cmu.edu>
2024-02-12 16:45:35 -0500

    Fix TS issues to pass lint

--- src/controllers/lists.ts
+++ src/controllers/lists.ts
@@ -1,9 +1,6 @@
-'use strict';
-import {Request, Response} from "express"
+import { Request, Response } from 'express';
 
-const listsController = module.exports;
-
-listsController.get = async function (req : Request, res : Response) {
+export default function get(req : Request, res : Response) {
     const listsData = {};
     res.render('lists', listsData);
-};
+}
