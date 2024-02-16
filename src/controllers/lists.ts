806799f40f7cd80ddf0571786bc33be5cc5d94ed
SongEmily <emsong@andrew.cmu.edu>
2024-02-15 19:33:17 -0500

    fix error starting server after ts translation

--- src/controllers/lists.ts
+++ src/controllers/lists.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
 
-export default function get(req : Request, res : Response) {
+export function get(req : Request, res : Response) {
     const listsData = {};
     res.render('lists', listsData);
+}
+
+export function test() {
+    return {};
 }
